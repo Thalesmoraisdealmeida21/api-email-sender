21f9e42525b35d4f2682472283a5fab38255c85e
Thales Morais de Almeida <thalesmoraisdealmeida@pop-os.localdomain>
2023-05-27 20:49:55 -0300

    Implemented mailjet

--- server.js
+++ server.js
@@ -2,38 +2,53 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const nodemailer = require("nodemailer");
+const axios = require('axios')
 require('dotenv').config()
+
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/sendmail/:toEmail', async (request, response) => {
+app.post('/sendmail/:toEmail', async (request, resp) => {
     const subject = request.body.subject
     const toEmail = request.params.toEmail
     const from = request.body.from
     const text = request.body.text
     const html = request.body.html
 
-
-    let transporter = nodemailer.createTransport({
-        host: 'smtp-mail.outlook.com',
-        port: 587,
-        secure: false,
-        auth: {
-          user: 'thalesmoraisdealmeida@outlook.com', 
-          pass: '#vmwA>O>o~Bf"I9+v[cz'
+    try {
+      const response = await axios.post(
+        'https://api.mailjet.com/v3/send',
+         {
+          'FromEmail': 'thales.morais21@gmail.com',
+          'FromName': 'Your Mailjet Pilot',
+          'Recipients': [
+            {
+              'Email': 'thales.morais21@gmail.com',
+              'Name': 'Thales Morais'
+            }
+          ],
+          'Subject': 'Your email flight plan!',
+          'Text-part': 'Dear passenger, welcome to Mailjet! May the delivery force be with you!',
+          'Html-part': '<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!'
         },
-      });
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          auth: {
+            username: 'a0d23373922398875c4288f6a02375a3',
+            password: '455de2060099cbbf6d9213beb5e76c47'
+          }
+        }
+      );
+
+      resp.status(200).json({status: 'ok'})
+
+    } catch(e) {
+      console.log("Deu erro")
+    }
     
-      let info = await transporter.sendMail({
-        from: from,
-        to: toEmail, 
-        subject: subject, 
-        text: text, 
-        html: html, 
-      });
-    
-      response.json({"status": "ok", "message": "sended", "idMessage": info.response}) 
 })
 
 
